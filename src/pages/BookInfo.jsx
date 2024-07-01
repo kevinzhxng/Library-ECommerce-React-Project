a96eb716d098b74fb37d1c4508b4927dd152629a
kevinzhxng <kzhang0826@gmail.com>
2024-07-01 17:02:58 -0400

    bug with tofixed can't add to cart

---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Rating from "../components/ui/Rating";
+import Price from "../components/ui/Price";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import Book from "../components/ui/Book";
+
+export default function BookInfo({ books, addToCart }) {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src={book.url} alt="" className="book__selected__img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
+                <div className="book__selected--price">
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
+                </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                    expedita porro perspiciatis nisi deserunt voluptate officiis
+                    facilis dolor delectus vel corrupti aut quod laborum
+                    reiciendis, tenetur reprehenderit est maxime aliquam.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                    expedita porro perspiciatis nisi deserunt voluptate officiis
+                    facilis dolor delectus vel corrupti aut quod laborum
+                    reiciendis, tenetur reprehenderit est maxime aliquam.
+                  </p>
+                </div>
+                <button className="btn" onClick={() => addToCart(book)}>Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
