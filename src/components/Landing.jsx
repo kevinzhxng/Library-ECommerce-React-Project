54b8e8081c54d1be9f3230549b5c72a2ac910a99
kevinzhxng <kzhang0826@gmail.com>
2024-07-03 19:41:37 -0400

    changed theme and color

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UndrawBooks from "../assets/Undraw_Books.svg";
+import UndrawBooks from "../assets/undraw_reading_list_re_bk72.svg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Landing() {
